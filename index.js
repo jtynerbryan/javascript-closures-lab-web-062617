8158b33aeac98c92e4742104589702cf4eccc701
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-08-08 17:26:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const app = "I don't do much."
+const { bumps } = bumpCounter();
+
+function bumpCounter() {
+	var counter = 0;
+
+	function addBump() {
+		counter += 1;
+	};
+
+	function getBumps() {
+		return counter;
+	};
+
+	return {
+    	addBump,
+    	getBumps
+  	};
+};
+
+const { combo } = createAnimal();
+
+function createAnimal(animalType) {
+
+	function createcombination(deadlyDevice) {
+		return {
+			animalType,
+			deadlyDevice
+		};
+	};
+
+	return createcombination
+};
+
+var sharkCreator = createAnimal("Shark");
+
+var sharkWithFrickinLaserbeam = createAnimal('Shark')('Laserbeam');
+
+var sharkWithFrickinCannon = createAnimal('Shark')('Cannon');
